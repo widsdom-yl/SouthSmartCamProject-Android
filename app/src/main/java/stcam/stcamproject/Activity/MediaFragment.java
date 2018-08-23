897fed82c5ef5752ace2ccd9a203b1fb9242381c
ZHB\zhb <4719373@qq.com>
2018-08-23 11:08:34 +0800

    V1.3.0

--- app/src/main/java/stcam/stcamproject/Activity/MediaFragment.java
+++ app/src/main/java/stcam/stcamproject/Activity/MediaFragment.java
@@ -2,6 +2,7 @@ package stcam.stcamproject.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
